c31b5010e6af4f18d2233e366be8bc6e47240284
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-04-11 07:50:27 -0500

    rest_dbus: allow route '/upload/image/<filename>'
    This allows the user to indicate to the BMC to save the upload file with
    it's original name, or with a custom name :
    
    'curl <options> -X POST -T foo https://<bmc_IP>/upload/image/'
    - Note the '/' after upload. This will save the file foo with the same
      name on the BMC.
    
    'curl <options> -X POST -T foo https://<bmc_IP>/upload/image/bar'
    - This will save the file foo with the name bar on the BMC.
    
    Resolves openbmc/openbmc#1332.
    
    Change-Id: Ie3d04617114ec574c573ae0ac9ecf65769800062
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

--- module/obmc/wsgi/apps/rest_dbus.py
+++ module/obmc/wsgi/apps/rest_dbus.py
@@ -538,7 +538,7 @@ class ImageUploadHandler(RouteHandler):
     ''' Handles the /upload route. '''
 
     verbs = ['POST']
-    rules = ['/upload/image']
+    rules = ['/upload/image', '/upload/image/<filename>']
     content_type = 'application/octet-stream'
     file_name = 'image'
     file_num = 0
@@ -548,10 +548,12 @@ class ImageUploadHandler(RouteHandler):
         super(ImageUploadHandler, self).__init__(
             app, bus, self.verbs, self.rules, self.content_type)
 
-    def do_post(self, **kw):
+    def do_post(self, filename=''):
         if not os.path.exists(self.file_loc):
             os.makedirs(self.file_loc)
-        filename = self.file_name + str(self.file_num)
+        if not filename:
+            filename = self.file_name + str(self.file_num)
+            self.file_num += 1
 
         if py3:
             try:
@@ -566,7 +568,6 @@ class ImageUploadHandler(RouteHandler):
             else:
                 with open(file, "w") as fd:
                     fd.write(request.body.read())
-        self.file_num += 1
 
     def find(self, **kw):
         pass

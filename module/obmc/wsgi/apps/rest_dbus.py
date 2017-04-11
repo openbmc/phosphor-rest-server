6c188599db3ec48955cd6e943f2f7590f25a8029
Deepak Kodihalli <dkodihal@in.ibm.com>
2017-04-11 07:50:27 -0500

    rest_dbus: allow route '/upload/image/<filename>'
    This allows the user to provide a filename for the uploaded image.
    
    'curl <options> -X PUT -T foo https://<bmc_IP>/upload/image/bar'
    - This will save the file foo with the name bar on the BMC.
    
    Resolves openbmc/openbmc#1332.
    
    Change-Id: Ie3d04617114ec574c573ae0ac9ecf65769800062
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

--- module/obmc/wsgi/apps/rest_dbus.py
+++ module/obmc/wsgi/apps/rest_dbus.py
@@ -531,31 +531,59 @@ class SessionHandler(MethodHandler):
         pass
 
 
-class ImageUploadHandler(RouteHandler):
-    ''' Handles the /upload route. '''
+class ImageUploadUtils:
+    ''' Provides common utils for image upload. '''
 
-    verbs = ['POST']
-    rules = ['/upload/image']
-    content_type = 'application/octet-stream'
     file_name = 'image'
     file_num = 0
     file_loc = '/tmp/images'
 
+    @classmethod
+    def do_upload(cls, filename=''):
+        if not os.path.exists(cls.file_loc):
+            os.makedirs(cls.file_loc)
+        if not filename:
+            filename = cls.file_name + str(cls.file_num)
+            cls.file_num += 1
+
+        with open(os.path.join(cls.file_loc, filename), "w") as fd:
+            fd.write(request.body.read())
+
+
+class ImagePostHandler(RouteHandler):
+    ''' Handles the /upload/image route. '''
+
+    verbs = ['POST']
+    rules = ['/upload/image']
+    content_type = 'application/octet-stream'
+
     def __init__(self, app, bus):
-        super(ImageUploadHandler, self).__init__(
+        super(ImagePostHandler, self).__init__(
             app, bus, self.verbs, self.rules, self.content_type)
 
-    def do_post(self, **kw):
-        self.do_upload(**kw)
+    def do_post(self, filename=''):
+        ImageUploadUtils.do_upload()
 
-    def do_upload(self, **kw):
-        if not os.path.exists(self.file_loc):
-            os.makedirs(self.file_loc)
-        filename = self.file_name + str(self.file_num)
+    def find(self, **kw):
+        pass
 
-        with open(os.path.join(self.file_loc, filename), "w") as fd:
-            fd.write(request.body.read())
-        self.file_num += 1
+    def setup(self, **kw):
+        pass
+
+
+class ImagePutHandler(RouteHandler):
+    ''' Handles the /upload/image/<filename> route. '''
+
+    verbs = ['PUT']
+    rules = ['/upload/image/<filename>']
+    content_type = 'application/octet-stream'
+
+    def __init__(self, app, bus):
+        super(ImagePutHandler, self).__init__(
+            app, bus, self.verbs, self.rules, self.content_type)
+
+    def do_put(self, filename=''):
+        ImageUploadUtils.do_upload(filename)
 
     def find(self, **kw):
         pass
@@ -869,7 +897,8 @@ class App(Bottle):
         self.method_handler = MethodHandler(self, self.bus)
         self.property_handler = PropertyHandler(self, self.bus)
         self.schema_handler = SchemaHandler(self, self.bus)
-        self.image_upload_handler = ImageUploadHandler(self, self.bus)
+        self.image_upload_post_handler = ImagePostHandler(self, self.bus)
+        self.image_upload_put_handler = ImagePutHandler(self, self.bus)
         self.instance_handler = InstanceHandler(self, self.bus)
 
     def install_handlers(self):
@@ -880,7 +909,8 @@ class App(Bottle):
         self.method_handler.install()
         self.property_handler.install()
         self.schema_handler.install()
-        self.image_upload_handler.install()
+        self.image_upload_post_handler.install()
+        self.image_upload_put_handler.install()
         # this has to come last, since it matches everything
         self.instance_handler.install()
 

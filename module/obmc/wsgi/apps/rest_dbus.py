7cc3f43e660ad5289deef4097b19351045bfd8d5
Marri Devender Rao <devenrao@in.ibm.com>
2017-11-20 00:15:47 -0600

    Fix BMC went into bad state, not responding to REST
    When trying to set IPADDRESS with invalid format, REST
    server tries to convert the type and perform a retry.
    
    The retry logic fails and continues to retry in a loop
    causing the REST server to fail.
    
    Change-Id: I150464de5585ebdfab4ae2da2083a62bd63caca1
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- module/obmc/wsgi/apps/rest_dbus.py
+++ module/obmc/wsgi/apps/rest_dbus.py
@@ -326,6 +326,7 @@ class MethodHandler(RouteHandler):
             app, bus, self.verbs, self.rules, self.content_type)
         self.service = ''
         self.interface = ''
+        self.retry = True
 
     def find(self, path, method):
         method_list = []
@@ -353,7 +354,7 @@ class MethodHandler(RouteHandler):
 
         except dbus.exceptions.DBusException, e:
             paramlist = []
-            if e.get_dbus_name() == DBUS_INVALID_ARGS:
+            if e.get_dbus_name() == DBUS_INVALID_ARGS and self.retry == True:
 
                 signature_list = get_method_signature(self.bus, self.service,
                                                       path, self.interface,
@@ -369,6 +370,7 @@ class MethodHandler(RouteHandler):
                         converted_value = convert_type(expected_type, value)
                         paramlist.append(converted_value)
                     request.parameter_list = paramlist
+                    self.retry = False
                     self.do_post(path, method)
                     return
                 except Exception as ex:
@@ -413,6 +415,7 @@ class PropertyHandler(RouteHandler):
     def __init__(self, app, bus):
         super(PropertyHandler, self).__init__(
             app, bus, self.verbs, self.rules, self.content_type)
+        self.retry = True
 
     def find(self, path, prop):
         self.app.instance_handler.setup(path)
@@ -447,7 +450,7 @@ class PropertyHandler(RouteHandler):
         except ValueError, e:
             abort(400, str(e))
         except dbus.exceptions.DBusException, e:
-            if e.get_dbus_name() == DBUS_INVALID_ARGS:
+            if e.get_dbus_name() == DBUS_INVALID_ARGS and self.retry == True:
                 bus_name = properties_iface.bus_name
                 expected_type = get_type_signature_by_introspection(self.bus,
                                                                     bus_name,
@@ -457,6 +460,7 @@ class PropertyHandler(RouteHandler):
                     abort(403, "Failed to get expected type: %s" % str(e))
                 converted_value = None
                 try:
+                    self.retry = False
                     converted_value = convert_type(expected_type, value)
                     self.do_put(path, prop, converted_value)
                     return

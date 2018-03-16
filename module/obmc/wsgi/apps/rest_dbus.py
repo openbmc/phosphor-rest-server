e4b9328c4d61b230a2a1bf08836123947f026871
Adriana Kobylak <anoo@us.ibm.com>
2018-03-16 13:29:41 -0500

    rest_dbus: do_upload: Add version id error code
    If there was no D-Bus object created, it means that either the version
    already existed, or there was a failure extracting the file. Ideally the
    REST server would query to see if the version already exists, but it
    doesn't know the version id to look for, so adding a single error msg
    for all error cases. It still makes sense to have a 400 (client) error
    for tar errors since a failure to untar is most likely an invalid or
    corrupted file that the user would need to address.
    
    Tested:
    Uploading a regular file (that triggers a tar failure) or a
    version that already exists on the system:
    Before:
      {
        "data": null,
        "message": "200 OK",
        "status": "ok"
      }
    
    After:
      {
        "data": {
          "description": "Version already exists or failed to be extracted"
        },
        "message": "400 Bad Request",
        "status": "error"
      }
    
    Fixes openbmc/openbmc#2939
    
    Change-Id: Ia4be5fe1dac3c2c7ebb5eb2aa28e4d58f9222c7f
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- module/obmc/wsgi/apps/rest_dbus.py
+++ module/obmc/wsgi/apps/rest_dbus.py
@@ -816,7 +816,11 @@ class ImagePostHandler(RouteHandler):
             app, bus, self.verbs, self.rules, self.content_type)
 
     def do_post(self, filename=''):
-        return ImageUploadUtils.do_upload()
+        upload_response = ImageUploadUtils.do_upload()
+        if upload_response is None:
+            abort(400, "Version already exists or failed to be extracted")
+        else:
+            return upload_response
 
     def find(self, **kw):
         pass
@@ -938,7 +942,11 @@ class ImagePutHandler(RouteHandler):
             app, bus, self.verbs, self.rules, self.content_type)
 
     def do_put(self, filename=''):
-        return ImageUploadUtils.do_upload(filename)
+        upload_response = ImageUploadUtils.do_upload(filename)
+        if upload_response is None:
+            abort(400, "Version already exists or failed to be extracted")
+        else:
+            return upload_response
 
     def find(self, **kw):
         pass

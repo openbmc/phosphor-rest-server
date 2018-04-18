7507d07313964cb7c6aca5eddcb8df97753c0b4d
Nagaraju Goruganti <ngorugan@in.ibm.com>
2018-04-17 22:27:08 -0500

    Provide the infrastructure to whitelist given URL from REST server
    Added a plug-in which runs on each request and checks if the requested
    URL consists of whitelisted URL, if so, allows the access, otherwise
    fails with an error message.
    
    It gets whitelisted URL info from json file.
    
    Resolves openbmc/openbmc#2378
    
    Change-Id: I95e5fd080e03616a1cba2b86d951414669338b08
    Signed-off-by: Nagaraju Goruganti <ngorugan@in.ibm.com>

--- module/obmc/wsgi/apps/rest_dbus.py
+++ module/obmc/wsgi/apps/rest_dbus.py
@@ -30,6 +30,7 @@ import crypt
 import tempfile
 import re
 import mimetypes
+import fnmatch
 have_wsock = True
 try:
     from geventwebsocket import WebSocketError
@@ -1333,6 +1334,34 @@ class ContentCheckerPlugin(object):
 
         return self.Checker(content_type, callback)
 
+class CheckURLPlugin(object):
+    ''' Ensures that anything read and written using only urls listed in
+        the url_config.json config file would allowed. '''
+    name = 'url_checker'
+    api = 2
+
+    def __init__(self):
+        config_path = '/usr/share/rest-dbus/url_config.json'
+        url_config = {}
+        self.urls = {}
+        if os.path.exists(config_path):
+            try:
+                with open(config_path) as data_file:
+                    url_config = json.load(data_file)
+                    self.urls = url_config['urls']
+            except ValueError as e:
+                    abort(404, str(e))
+        else:
+            abort(404, "Config file path not found for Whitelisted URLs")
+
+    def apply(self, callback, route):
+
+        def wrap(*a, **kw):
+                for url in self.urls:
+                    if fnmatch.fnmatch(request.path,url):
+                        return callback(*a, **kw)
+                abort(404,"Trying to access Blocked URL")
+        return wrap
 
 class App(Bottle):
     def __init__(self, **kw):
@@ -1360,6 +1389,7 @@ class App(Bottle):
         self.install(JsonApiResponsePlugin(self))
         self.install(JsonApiRequestPlugin())
         self.install(JsonApiRequestTypePlugin())
+        self.install(CheckURLPlugin())
 
     def install_hooks(self):
         self.error_handler_type = type(self.default_error_handler)

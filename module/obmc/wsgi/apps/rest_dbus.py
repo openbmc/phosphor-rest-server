7b52f367f2af1233ab20811fd7962347a37c0938
Ratan Gupta <ratagupt@in.ibm.com>
2018-09-04 09:24:25 +0530

    Enable PAM authentication in rest server
    Openbmc stack would be supporting LDAP as authentication module
    so bring PAM support in the rest server.
    
    PAM support in the rest server enables the rest server to use local linux
    authentication module, LDAP or both depends on the PAM configuration.
    
    Change-Id: I92b87d4fc864aade592ebc2c127921db9b4ae92e
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- module/obmc/wsgi/apps/rest_dbus.py
+++ module/obmc/wsgi/apps/rest_dbus.py
@@ -25,9 +25,9 @@ from bottle import static_file
 import obmc.utils.misc
 from obmc.dbuslib.introspection import IntrospectionNodeParser
 import obmc.mapper
-import spwd
 import grp
-import crypt
+import pamela
+from pamela import PAMError
 import tempfile
 import re
 import mimetypes
@@ -669,14 +669,6 @@ class SessionHandler(MethodHandler):
         self.hmac_key = os.urandom(128)
         self.session_store = []
 
-    @staticmethod
-    def authenticate(username, clear):
-        try:
-            encoded = spwd.getspnam(username)[1]
-            return encoded == crypt.crypt(clear, encoded)
-        except KeyError:
-            return False
-
     def invalidate_session(self, session):
         try:
             self.session_store.remove(session)
@@ -723,8 +715,20 @@ class SessionHandler(MethodHandler):
         if len(request.parameter_list) != 2:
             abort(400, self.bad_json_str % (request.json))
 
-        if not self.authenticate(*request.parameter_list):
-            abort(401, self.bad_passwd_str)
+        try:
+            user = request.parameter_list[0]
+            password = request.parameter_list[1]
+            pamela.authenticate(user, password, 'restserver')
+            # check the account status whether account is expired or
+            # the authentication token / pasword is expired.
+            pamela.check_account(user)
+        except PAMError as e:
+            # Returning the pam message to the user otherwise need to
+            # define all the errors in the python context and map the
+            # pam message to the user friendly message.
+            # error string would be like as below
+            # "[PAM Error 7] Authentication failure"
+            abort(401, e)
 
         force = False
         try:

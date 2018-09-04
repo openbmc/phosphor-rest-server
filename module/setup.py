14dc2fbbc4b4c9a5a284b7fc44ccd9dd3dacdde7
Ratan Gupta <ratagupt@in.ibm.com>
2018-09-04 09:24:25 +0530

    Enable PAM authentication in rest server
    Openbmc stack would be supporting LDAP as authentication module
    so bring PAM support in the rest server.
    
    PAM support in the rest server enables the rest server to use local linux
    authentication module, LDAP or both depends on the PAM configuration.
    
    Tested:
    LDAP Enabled:
            Login with valid linux credentials.
               PASS
            Login with invalid linux credentails.
               FAIL with message "Authentication Failure"
            Login with valid ldap credentials.
               PASS
    
    LDAP Disabled
            Login with valid linux credentials.
               PASS
            Login with valid ldap credentials.
               FAIL with message "Authentication Failure"
    common
           Login with bad types for username and password
               PASS: got the 400 bad request
    
    Change-Id: I92b87d4fc864aade592ebc2c127921db9b4ae92e
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- module/setup.py
+++ module/setup.py
@@ -4,4 +4,5 @@ setup(
     name='phosphor-rest-dbus',
     version='1.0',
     py_modules=['obmc.wsgi.apps.rest_dbus'],
+    data_files=[('/etc/pam.d/',['obmc/wsgi/apps/phosphor-rest-server-ldap','obmc/wsgi/apps/phosphor-rest-server-linux','obmc/wsgi/apps/phosphor-rest-server'])],
     )

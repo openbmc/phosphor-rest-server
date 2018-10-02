c0af10f372227a3628c966fe515393dacc6ca02d
Deepak Kodihalli <dkodihal@in.ibm.com>
2018-10-02 06:27:57 -0500

    certificate handler: use mapper
    Use mapper to retrieve a D-Bus busname, instead of assuming a well-known
    busname.
    
    Change-Id: I668f750ab098b847bf3a662abdf6614170471c41
    Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

--- module/obmc/wsgi/apps/rest_dbus.py
+++ module/obmc/wsgi/apps/rest_dbus.py
@@ -884,7 +884,7 @@ class CertificateHandler:
     CERT_PATH = '/xyz/openbmc_project/certs'
     CERT_IFACE = 'xyz.openbmc_project.Certs.Install'
 
-    def do_upload(cls, cert_type, service):
+    def do_upload(cls, route_handler, cert_type, service):
         def cleanup():
             if os.path.exists(temp.name):
                 os.remove(temp.name)
@@ -911,13 +911,14 @@ class CertificateHandler:
 
         try:
             bus = dbus.SystemBus()
-            busName = cls.CERT_BUSNAME + "." + cert_type.capitalize() + "." \
-                + service.capitalize()
             certPath = cls.CERT_PATH + "/" + cert_type + "/" + service
+            intfs = route_handler.try_mapper_call(
+                route_handler.mapper.get_object, path=certPath)
+            busName = [k for k,v in intfs.items() if cls.CERT_IFACE in v][0]
             obj = bus.get_object(busName, certPath)
             iface = dbus.Interface(obj, cls.CERT_IFACE)
             iface.Install(temp.name)
-        except dbus.exceptions.DBusException as e:
+        except Exception as e:
             cleanup()
             abort(503, str(e))
         cleanup()
@@ -935,7 +936,7 @@ class CertificatePutHandler(RouteHandler):
             app, bus, self.verbs, self.rules, self.content_type)
 
     def do_put(self, cert_type, service):
-        return CertificateHandler().do_upload(cert_type, service)
+        return CertificateHandler().do_upload(self, cert_type, service)
 
     def find(self, **kw):
         pass
